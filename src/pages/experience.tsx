fbd62c25db27b993cd90ca179464a4fd5814830a
Joyce Lee <joycejlee13@gmail.com>
2023-10-14 13:40:38 -0400

    fix spelling errors

--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -4,12 +4,12 @@ import React from 'react'
 
 const Experience = () => {
   const values = {
-    title : "My Experience",
+    title : "Experience",
     c1 : "Cognizant",
     p1: "August 2022 - October 2023",
     p2: "Full Stack Software Developer",
     p3: "Collaborated with other associate developers within the company on creating an internal application to display cross-company metrics of various on-going projects.",
-    p4: "Developed and maintained an android mobile application within a team of 3 for former stackholders."
+    p4: "Developed and maintained an android mobile application within a team of 3 for former stakeholders."
   }
   
   return (
